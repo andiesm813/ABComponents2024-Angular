c3867603b6cc70d64d9e27d5000242dd8218eaae
andiesm813 <86068582+andiesm813@users.noreply.github.com>
2024-07-15 18:32:07 -0300

    ⭐ All AB Components 2024 ⭐ updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './error-routing/not-found/not-found.component';
 import { UncaughtErrorComponent } from './error-routing/error/uncaught-error.component';
 import { AllComponentsComponent } from './all-components/all-components.component';
+import { FormsComponent } from './forms/forms.component';
 import { RadiogroupComponent } from './radiogroup/radiogroup.component';
 import { StepperComponent } from './stepper/stepper.component';
 
@@ -9,6 +10,7 @@ export const routes: Routes = [
   { path: '', redirectTo: 'all-components', pathMatch: 'full' },
   { path: 'error', component: UncaughtErrorComponent },
   { path: 'all-components', component: AllComponentsComponent, data: { text: 'AllComponents' } },
+  { path: 'forms', component: FormsComponent, data: { text: 'Forms' } },
   { path: 'radiogroup', component: RadiogroupComponent, data: { text: 'Radiogroup' } },
   { path: 'stepper', component: StepperComponent, data: { text: 'Stepper' } },
   { path: '**', component: PageNotFoundComponent } // must always be last
---
+++ src/app/forms/forms.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IGX_DATE_PICKER_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_SELECT_DIRECTIVES, IGX_SIMPLE_COMBO_DIRECTIVES, IgxDateTimeEditorModule, IgxIconComponent } from '@infragistics/igniteui-angular';
+import { Subject, takeUntil } from 'rxjs';
+import { BoxOfficeRevenueType } from '../models/financial/box-office-revenue-type';
+import { FinancialService } from '../services/financial.service';
+
+@Component({
+  selector: 'app-forms',
+  standalone: true,
+  imports: [IGX_SIMPLE_COMBO_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IGX_DATE_PICKER_DIRECTIVES, IGX_SELECT_DIRECTIVES, IgxDateTimeEditorModule, IgxIconComponent, FormsModule, FormsModule],
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.scss']
+})
+export class FormsComponent implements OnInit, OnDestroy {
+  private destroy$: Subject<void> = new Subject<void>();
+  public value: string = '5';
+  public value1: number = 10;
+  public value2: string = 'vvxcvxcvxcv';
+  public value3: string = 'cvxcvxcvxc';
+  public value4: string = '1';
+  public financialBoxOfficeRevenue: BoxOfficeRevenueType[] = [];
+
+  constructor(
+    private financialService: FinancialService,
+  ) {}
+
+  ngOnInit() {
+    this.financialService.getBoxOfficeRevenue().pipe(takeUntil(this.destroy$)).subscribe({
+      next: (data) => this.financialBoxOfficeRevenue = data,
+      error: (_err: any) => this.financialBoxOfficeRevenue = []
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
