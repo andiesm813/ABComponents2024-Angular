c0a37a97d8bec1cf5bb3f9f32e220066a7cb5a72
andiesm813 <86068582+andiesm813@users.noreply.github.com>
2024-07-15 17:48:59 -0300

    ⭐ All AB Components 2024 ⭐ updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { MasterViewComponent } from './master-view/master-view.component';
-import { IGX_NAVBAR_DIRECTIVES, IgxIconButtonDirective, IgxRippleDirective, IgxIconComponent, IGX_NAVIGATION_DRAWER_DIRECTIVES, IGX_TABS_DIRECTIVES, IGX_ACCORDION_DIRECTIVES, IGX_EXPANSION_PANEL_DIRECTIVES, IGX_LIST_DIRECTIVES, IgxAvatarComponent, IGX_TREE_DIRECTIVES, IGX_SLIDER_DIRECTIVES, IGX_STEPPER_DIRECTIVES, IgxButtonDirective, IGX_CARD_DIRECTIVES, IgxDividerDirective, IGX_INPUT_GROUP_DIRECTIVES, IgxToggleDirective, IgxToggleActionDirective, IgxOverlayOutletDirective, IGX_BUTTON_GROUP_DIRECTIVES, IgxBadgeComponent, IgxCheckboxComponent, IgxRadioComponent, IgxRadioGroupDirective, IgxSwitchComponent, IgcFormsModule, IGX_DATE_PICKER_DIRECTIVES, IGX_SELECT_DIRECTIVES, IGX_SIMPLE_COMBO_DIRECTIVES, IGX_CALENDAR_DIRECTIVES, IGX_GRID_DIRECTIVES, IgxSnackbarComponent, IGX_DIALOG_DIRECTIVES, IGX_BANNER_DIRECTIVES } from '@infragistics/igniteui-angular';
-import { FormsModule } from '@angular/forms';
-import { IgxCategoryChartModule } from 'igniteui-angular-charts';
-import { RadioGroupComponent } from './radio-group/radio-group.component';
-import { StepperComponent } from './stepper/stepper.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { IGX_NAVBAR_DIRECTIVES, IGX_NAVIGATION_DRAWER_DIRECTIVES, IgxIconButtonDirective, IgxIconComponent, IgxOverlayOutletDirective, IgxRippleDirective, IgxToggleActionDirective, IgxToggleDirective } from '@infragistics/igniteui-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [IGX_NAVIGATION_DRAWER_DIRECTIVES, IGX_NAVBAR_DIRECTIVES, IgxIconButtonDirective, IgxToggleActionDirective, IgxOverlayOutletDirective, IgxRippleDirective, IgxToggleDirective, IgxIconComponent, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Home - IgniteUI for Angular';
 }
--- src/app/radiogroup/radiogroup.component.ts
+++ src/app/radiogroup/radiogroup.component.ts
@@ -3,12 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { IgxRadioComponent, IgxRadioGroupDirective } from '@infragistics/igniteui-angular';
 
 @Component({
-  selector: 'app-radio-group',
+  selector: 'app-radiogroup',
   standalone: true,
   imports: [IgxRadioGroupDirective, IgxRadioComponent, FormsModule],
-  templateUrl: './radio-group.component.html',
-  styleUrls: ['./radio-group.component.scss']
+  templateUrl: './radiogroup.component.html',
+  styleUrls: ['./radiogroup.component.scss']
 })
-export class RadioGroupComponent {
+export class RadiogroupComponent {
   public value: string = '1';
 }
