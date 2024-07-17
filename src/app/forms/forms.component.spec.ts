3c6a777ac4f31e3037b6c518a3213f25ac451338
andiesm813 <86068582+andiesm813@users.noreply.github.com>
2024-07-17 20:41:53 -0300

    ⭐ All AB Components 2024 ⭐ updates

--- src/app/forms/forms.component.spec.ts
+++ src/app/forms/forms.component.spec.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-import { IGX_INPUT_GROUP_DIRECTIVES, IgxIconComponent, IgxDateTimeEditorModule, IGX_DATE_PICKER_DIRECTIVES, IGX_SELECT_DIRECTIVES, IGX_SIMPLE_COMBO_DIRECTIVES } from '@infragistics/igniteui-angular';
+import { IGX_INPUT_GROUP_DIRECTIVES, IgxIconComponent, IgxDateTimeEditorModule, IgxInputGroupModule, IGX_DATE_PICKER_DIRECTIVES, IGX_SELECT_DIRECTIVES, IGX_SIMPLE_COMBO_DIRECTIVES } from '@infragistics/igniteui-angular';
 import { FormsComponent } from './forms.component';
 
 describe('FormsComponent', () => {
@@ -13,7 +13,7 @@ describe('FormsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ FormsComponent ],
-      imports: [ NoopAnimationsModule, FormsModule, HttpClientTestingModule, IGX_INPUT_GROUP_DIRECTIVES, IgxIconComponent, IgxDateTimeEditorModule, IGX_DATE_PICKER_DIRECTIVES, IGX_SELECT_DIRECTIVES, IGX_SIMPLE_COMBO_DIRECTIVES ]
+      imports: [ NoopAnimationsModule, FormsModule, HttpClientTestingModule, IGX_INPUT_GROUP_DIRECTIVES, IgxIconComponent, IgxDateTimeEditorModule, IgxInputGroupModule, IGX_DATE_PICKER_DIRECTIVES, IGX_SELECT_DIRECTIVES, IGX_SIMPLE_COMBO_DIRECTIVES ]
     })
     .compileComponents();
   });
